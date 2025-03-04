24d5b4206c26874142184c49d44f1e49af6f85cc
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2025-03-04 14:24:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,19 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+  <h1> Welcome to {{title}}</h1>
+  <p>Hello world</p>
+  <router-outlet/>
+  `,
+  styles: [
+    `
+    p{
+    background-color:red;
+    }`
+  ]
 })
 export class AppComponent {
   title = 'my-angular-app';
